dd97be815d6f7aee1656239fece91302a6587127
Ken Sheedlo <ken.sheedlo@rackspace.com>
2015-03-20 10:53:52 -0700

    add SSO endpoint

--- sins.py
+++ sins.py
@@ -18,6 +18,20 @@ args = parser.parse_args()
 with open(args.config or 'config.json', 'r') as f:
     config = json.load(f)
 
+@app.route('/sso/', methods=['POST'])
+def start_sso():
+    saml_request = request.form['SAMLRequest']
+    relay_state = request.form['RelayState']
+
+    return redirect(url_for('login',
+        SAMLRequest=saml_request,
+        RelayState=relay_state,
+        next='/sso/'))
+
+@app.route('/', methods=['GET'])
+def login():
+    pass
+
 @app.route('/login', methods=['GET', 'POST'])
 def root():
     if request.method == 'GET':
